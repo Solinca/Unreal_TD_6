bded31aad87a3c9989b34e4b7cdced0bfbd09267
SamyMENA-BOUR <61056787+SamyMENA-BOUR@users.noreply.github.com>
2026-02-26 09:00:13 +0100

    Add PolygonParticleFX assets and update player
    Add a large PolygonParticleFX asset pack (blueprints, materials, meshes, particles, textures, demo map) and modify existing assets (BP_PlayerObjective, BP_Survivor, BaseLevel, NS_Lighting). Also update native code for player objective (PlayerObjective.cpp/.h) and adjust TD_6.Build.cs to accommodate the changes (build/module updates). This integrates the new particle FX content and applies related player/objective fixes.

--- Source/TD_6/TD_6.Build.cs
+++ Source/TD_6/TD_6.Build.cs
@@ -8,7 +8,7 @@ public class TD_6 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Slate", "SlateCore", "OnlineSubsystem", "OnlineSubsystemUtils", "NetCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "Slate", "SlateCore", "OnlineSubsystem", "OnlineSubsystemUtils", "NetCore", "Niagara", "NiagaraCore" });
 
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
