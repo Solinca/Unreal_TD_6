9a99a0296665a92090763600eab0eebf6636f389
SamyMENA-BOUR <61056787+SamyMENA-BOUR@users.noreply.github.com>
2026-03-03 15:58:56 +0100

    Update MyCharacter.cpp

--- Source/TD_6/Private/Player/MyCharacter.cpp
+++ Source/TD_6/Private/Player/MyCharacter.cpp
@@ -45,7 +45,7 @@ void AMyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 
-	GetMesh()->OnComponentHit.AddDynamic(this, &AMyCharacter::OnHit);
+	GetCapsuleComponent()->OnComponentHit.AddDynamic(this, &AMyCharacter::OnHit);
 
 	if (IsLocallyControlled())
 	{
